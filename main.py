a13d5a08bb9cba88ee5299cac84b720df792488a
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2022-10-17 09:50:12 -0700

    updated main.py to capture PC output... maybe undo later...

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+import computer_project.computer
 import gui_basics_project.gui_basics
 
-def test_root_not_none():
-    test_gui = gui_basics_project.gui_basics.Gui_basics()
-    print(f"type(test_gui.root): {type(test_gui.root)}")
-
-test_root_not_none()
+my_pc = computer_project.computer.Computer()
+my_pc.toggle_power()
+print(my_pc)
 
 # my_gui = gui_basics_project.gui_basics.Gui_basics()
 # my_gui.update_label_text("This is some new text!")
 
 # my_gui.mainloop()
 
-print("Got to the end of main!")
+# debug
+# print("Got to the end of main!")
 
 '''
 ToDo:
