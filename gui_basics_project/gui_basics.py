a5c219f5d5293df6ee8030d2be09b1d49c9de9ff
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2022-10-19 13:20:13 -0700

    Move tests, add sub-window to GUI, and add keyboard interaction.

--- gui_basics_project/gui_basics.py
+++ gui_basics_project/gui_basics.py
@@ -4,6 +4,10 @@ import tkinter as tk
 
 from computer_project import computer
 
+"""
+    ToDo:
+        have errors in label on GUI blink before disappearing
+"""
 
 class Gui_basics:
     '''Show some basic GUI components and layout capabilities.'''
@@ -15,8 +19,63 @@ class Gui_basics:
         
         self.computer_label_string_var.set(new_text)
 
-    def move_canavs_object(self, object_index, move_x, move_y):
-        self.canvas_output.move(object_index, move_x, move_y)
+    def move_canavs_object(self, move_x, move_y):
+        object_index = self.input_text.get('1.0', 'end').strip()
+
+        if object_index.isnumeric():
+            object_index = int(object_index)
+            if object_index > 0 and object_index <= len(self.canvas_object_ids):
+                self.canvas_output.move(object_index, move_x, move_y)
+            else:
+                # should this show up on the GUI and blink a few times before disappearing?
+                print(f"object index is out of range ({object_index})")
+        elif object_index == "all":
+            for index in self.canvas_object_ids:
+                self.canvas_output.move(index, move_x, move_y)
+        else:
+            print(f"object index is invalid ({object_index})")
+
+
+    def create_new_window(self):
+        """Create a new window with a label on it."""
+        # should we create a list of windows for our program!?
+        if self.new_window is None:
+            print(f"Creating a new window!")
+            self.new_window = tk.Toplevel(self.root)
+            self.new_window.title("a new window")
+            self.new_window.geometry("200x200")
+            
+            new_label = tk.Label(self.new_window, text="a new label on a new window!")
+            new_label.pack()
+
+            show_main_window_button = tk.Button(self.new_window, text="show main window", command=self.show_main_window)
+            show_main_window_button.pack()
+
+            self.new_window_text = tk.Text(self.new_window, width=10)
+            self.new_window_text.pack()
+        else:
+            self.new_window.deiconify()
+
+        self.root.withdraw()
+        self.new_window_text.focus_force()
+
+    def show_main_window(self):
+        self.root.deiconify()
+        self.new_window.withdraw()
+        self.input_text.focus_force()
+
+    def arrow_handler(self, event):
+        # debug
+        # print(f"event: {event}")
+        if event.keysym == "Left":
+            self.move_canavs_object(-10, 0)
+        elif event.keysym == "Right":
+            self.move_canavs_object(10, 0)
+        elif event.keysym == "Up":
+            self.move_canavs_object(0, -10)
+        elif event.keysym == "Down":
+            self.move_canavs_object(0, 10)
+
 
     # setup GUI components
     # this is called the View of a program in the Model-View-Controller (MVC) architecture
@@ -27,7 +86,8 @@ class Gui_basics:
         self.root.title("Computer GUI")
 
         self.computer_label_string_var = tk.StringVar(self.root, "hello?")
-        self.computer_label = tk.Label(self.root, textvariable=self.computer_label_string_var, width=20)
+        zero_img = tk.PhotoImage() # zero sized image to set scale of label to pixels instead of character width and height...
+        self.computer_label = tk.Label(self.root, textvariable=self.computer_label_string_var, image=zero_img, compound=tk.CENTER, width=1000, height=200)
         self.computer_label.grid(column=0, row=0)
 
         self.label_a1 = tk.Label(self.root, text="A1")
@@ -46,29 +106,43 @@ class Gui_basics:
         self.update_label_button.grid(column=1, row=2)
 
         # this code is easy to break, because we are not validating input yet...!
-        self.move_canavs_object_right_button = tk.Button(self.root, text="Right", command=lambda: self.move_canavs_object(int(self.input_text.get('1.0', 'end')), 10, 0))
+        self.move_canavs_object_right_button = tk.Button(self.root, text="Right", command=lambda: self.move_canavs_object(10, 0))
         self.move_canavs_object_right_button.grid(column=5, row=2)
 
         # this code is easy to break, because we are not validating input yet...!
-        self.move_canavs_object_left_button = tk.Button(self.root, text="Left", command=lambda: self.move_canavs_object(int(self.input_text.get('1.0', 'end')), -10, 0))
+        self.move_canavs_object_left_button = tk.Button(self.root, text="Left", command=lambda: self.move_canavs_object(-10, 0))
         self.move_canavs_object_left_button.grid(column=3, row=2)
 
         # this code is easy to break, because we are not validating input yet...!
-        self.move_canavs_object_up_button = tk.Button(self.root, text="Up", command=lambda: self.move_canavs_object(int(self.input_text.get('1.0', 'end')), 0, -10))
+        self.move_canavs_object_up_button = tk.Button(self.root, text="Up", command=lambda: self.move_canavs_object(0, -10))
         self.move_canavs_object_up_button.grid(column=4, row=1)
 
         # this code is easy to break, because we are not validating input yet...!
-        self.move_canavs_object_down_button = tk.Button(self.root, text="Down", command=lambda: self.move_canavs_object(int(self.input_text.get('1.0', 'end')), 0, 10))
+        self.move_canavs_object_down_button = tk.Button(self.root, text="Down", command=lambda: self.move_canavs_object(0, 10))
         self.move_canavs_object_down_button.grid(column=4, row=2)
 
         self.input_text = tk.Text(self.root, width=10, height=1)
         self.input_text.grid(column=2, row=0)
+        self.input_text.focus_set()
 
         self.canvas_output = tk.Canvas(self.root, width=640, height=480, background="black")
         self.canvas_output.grid(column=0, row=3, columnspan=3)
-        my_rectangle1_id = self.canvas_output.create_rectangle(10, 10, 20, 20, fill="red")
-        my_rectangle2_id = self.canvas_output.create_rectangle(30, 10, 40, 20, fill="green")
-        my_rectangle3_id = self.canvas_output.create_rectangle(10, 30, 20, 40, fill="blue")
+        self.canvas_object_ids = []
+        list_of_colors = ["red", "green", "blue"]
+        for count in range(0,6):
+            new_shape = self.canvas_output.create_rectangle(10 * count, 10, 10 * count + 10, 20, fill=list_of_colors[count % len(list_of_colors)])
+            self.canvas_object_ids.append(new_shape)
+
+        self.root.bind("<Key>", self.arrow_handler)
+
+
+
+        # new window stuff
+        self.new_window = None
+
+        # Button for creating a new window.
+        self.new_window_button = tk.Button(self.root, text="Show sub-window", command=self.create_new_window)
+        self.new_window_button.grid(column=2, row=1)
         
         # debug
         # print(f"my_rectangle1_id: {my_rectangle1_id}")
--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import computer_project.computer
 import gui_basics_project.gui_basics
 
-my_pc = computer_project.computer.Computer()
-my_pc.toggle_power()
-print(my_pc)
+# my_pc = computer_project.computer.Computer()
+# my_pc.toggle_power()
+# print(my_pc)
 
-# my_gui = gui_basics_project.gui_basics.Gui_basics()
-# my_gui.update_label_text("This is some new text!")
+my_gui = gui_basics_project.gui_basics.Gui_basics()
 
-# my_gui.mainloop()
-
-# debug
-# print("Got to the end of main!")
-
-'''
-ToDo:
-    Make class out of gui_basics.py and create a main method and main guard in this file?
-'''
+my_gui.mainloop()
